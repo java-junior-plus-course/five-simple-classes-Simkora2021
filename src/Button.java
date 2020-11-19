a1483e6222da521dbfcf85555f342434c898731a
Simkora <simkora1996@gmail.com>
2020-11-19 23:45:50 +0300

    complete class Button

---
+++ src/Button.java
@@ -0,0 +1,55 @@
+
+
+
+
+// 2 refresh method
+
+public class Button {
+    private String JButton0;
+    private String JButton1;
+    private String JButton2;
+    private String JButton3;
+    private String JButton4;
+    private String JButton5;
+    private String JButton6;
+    private String JButton7;
+    private String JButton8;
+    private String JButton9;
+
+
+    public String getJbutton0() {
+        return JButton0;
+    }
+
+    public void setJbutton0(String JButton0) {
+        this.JButton0 = JButton0;
+    }
+
+
+    public String getJbutton1() {
+        return JButton1;
+    }
+    public void setJbutton1(String JButton1) {
+        this.JButton1 = JButton1;
+    }
+    public class First{
+        void WriteNumberOne(){
+            System.out.println("Write one");
+        }
+    }
+    public class Second{
+        void WriteNumberTwo(){
+            System.out.println("Write two");
+        }
+    }
+
+        void WriteNumberThree(){
+            System.out.println("Write three");
+        }
+
+
+        void WriteNumberThree(String JButton3){
+            System.out.println("Write three");
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+// Схематично изобразите 5 классов
+// get & set
+// 5 method
+// 4 field
+// 2 refresh method
+
+
 public class Main {
 
     public static void main(String[] args) {
 
-	// write your code here
     }
 }
